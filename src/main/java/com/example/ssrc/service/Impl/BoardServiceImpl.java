36642914e850c52e320e449a76ffdac0a1033240
formulate <nervers1@gmail.com>
2022-01-19 11:20:35 +0900

    1. 게시글 관련 CRUD 작성 2. s-src log file change

--- src/main/java/com/example/ssrc/service/Impl/BoardServiceImpl.java
+++ src/main/java/com/example/ssrc/service/Impl/BoardServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.ssrc.service.Impl;
 
+import com.example.ssrc.domain.Post;
 import com.example.ssrc.mapper.BoardMapper;
 import com.example.ssrc.service.BoardService;
 import org.slf4j.Logger;
@@ -7,12 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class BoardServiceImpl implements BoardService {
     private static final Logger LOGGER = LoggerFactory.getLogger(BoardServiceImpl.class);
 
-    private BoardMapper mapper;
+    private final BoardMapper mapper;
 
     public BoardServiceImpl(BoardMapper mapper) {
         this.mapper = mapper;
@@ -20,8 +22,33 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public LocalDateTime getTime() {
-        LocalDateTime time = (LocalDateTime)mapper.getTime();
+        LocalDateTime time = mapper.getTime();
         LOGGER.info("시간이 찍히는지 봅시다 --> {}", time);
         return time;
+    }
+
+    @Override
+    public List<Post> list(Post post) {
+        return mapper.listPosts(post);
+    }
+
+    @Override
+    public Post getPost(Post post) {
+        return mapper.getPost(post);
+    }
+
+    @Override
+    public int createPost(Post post) {
+        return mapper.createPost(post);
+    }
+
+    @Override
+    public int modifyPost(Post post) {
+        return mapper.updatePost(post);
+    }
+
+    @Override
+    public int removePost(Post post) {
+        return mapper.deletePost(post);
     }
 }
