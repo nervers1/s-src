f9a1ef110c64c73faee56f2f7c8db8052ba794f8
formulate <nervers1@gmail.com>
2022-01-20 23:42:05 +0900

    1. 썸머 에디터로 이미지 업로드는 잘 되는데, 이미지가 base64로 인코딩 되어 html로 저장된다. 이렇게 되면 이미지가 커버리면 html 저장되는 컬럼 사이즈가 어마무시해지는데, 이걸 커버할 방법을 찾아야 한다. 2. 이미지를 업로드하고 업로드한 경로를 다시 ajax요청으로 전달해서 이미지 태그의 url을 변경해준다. --> 잘 되는데, 문제는 부트에서 리소스를 갱신하지 못한다. --> 신기하게 스프링 부트를 리붓하면 그 이미지가 보이고, 리붓 안하면 이미지를 못 찾아서 404 not found가 발생한다.
    3. 우선은 1번 버전으로 놔 두고 이미지 업로드 되는 것을 우선 확인한다.

--- src/main/java/com/example/ssrc/WebConfig.java
+++ src/main/java/com/example/ssrc/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
         registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/public/", "/images/", "/public", "classpath:/static/").setCachePeriod(31556926);
-        registry.addResourceHandler("/upload/**").addResourceLocations("/upload/").setCachePeriod(31556926);
+        registry.addResourceHandler("/upload/**").addResourceLocations("classpath:/static/upload/").setCachePeriod(31556926);
 //        registry.addResourceHandler("/m/**").addResourceLocations("classpath:/m/").setCachePeriod(20);
 //        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/").setCachePeriod(20);
 //        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(20);
--- src/main/java/com/example/ssrc/controller/BoardRestController.java
+++ src/main/java/com/example/ssrc/controller/BoardRestController.java
@@ -2,16 +2,21 @@ package com.example.ssrc.controller;
 
 import com.example.ssrc.domain.Post;
 import com.example.ssrc.service.BoardService;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.relational.core.sql.In;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class BoardRestController {
@@ -49,4 +54,43 @@ public class BoardRestController {
         }
         return resultMap;
     }
+
+
+    @PostMapping(value="/uploadSummernoteImageFile", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
+
+        LOGGER.info("----------------- upload ");
+        Map<String, Object> jsonObject = new HashMap<>();
+        // /opt/code/projects/s-src/src/main/resources/templates/test.html
+        // /opt/code/projects/s-src/src/main/resources/templates/test.html
+        String fileRoot = "/opt/code/projects/s-src/src/main/resources/static/upload/";	//저장될 외부 파일 경로
+//        String fileRoot = "classpath:/upload/";	//저장될 외부 파일 경로
+        String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
+        String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
+
+
+        File targetFile = new File(fileRoot + savedFileName);
+        try (InputStream fileStream = multipartFile.getInputStream()){
+
+            LOGGER.info("getOriginalFilename : {}", originalFileName);
+            LOGGER.info("extension : {}", extension);
+            LOGGER.info("targetFile : {}", targetFile);
+            LOGGER.info("savedFileName : {}", savedFileName);
+
+//            multipartFile.transferTo(targetFile);
+            FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
+            jsonObject.put("url", "/upload/" +savedFileName);
+            jsonObject.put("responseCode", "success");
+
+        } catch (IOException e) {
+            FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
+            jsonObject.put("responseCode", "error");
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok().body(jsonObject);
+    }
+
 }
